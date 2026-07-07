4f22c757cee275017ae78d266b2cf4ef076624e7
elrumo <elrumo@users.noreply.github.com>
2026-07-07 17:43:29 +0000

    Simplify Mongo creds: use compose var directly, drop shared secrets volume

--- parse/docker-entrypoint.sh
+++ parse/docker-entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 set -e
-
-SECRETS="/run/secrets/mongo.env"
-if [ -f "$SECRETS" ]; then
-  . "$SECRETS"
-  export PARSE_SERVER_DATABASE_URI="mongodb://${MONGO_USER}:${MONGO_PASSWORD}@mongo:27017/${MONGO_DB}?authSource=admin"
-fi
-
 exec node /parse-server/bin/parse-server "$@"
